3d58544c09acbc5edc8189178cf804f044141967
lyptyu <906641851@qq.com>
2025-05-27 14:41:52 +0800

    feat

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -60,7 +60,7 @@ export function bigDataQuery(data: { idCard: string, phone: string, name: string
 
 // 征信风控解析接口
 export function creditAnalysis(data: { url: string, analysisType: string }): Promise<ApiResponse> {
-  return request.post(`/credit/parse?url=${data.url}&analysisType=${data.analysisType}`)
+  return request.post(`/credit/parseSave?url=${data.url}&analysisType=${data.analysisType}`)
 }
 
 // 发送验证码接口
